d774d8c6d2b578c1a7b3e8af582abc253ec95cdf
Sufiya Sayyed <ssayyed01@gmail.com>
2025-10-22 19:03:51 +0400

    components and basic structure of portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import "./App.css";
+import ConnectSection from "./components/ConnectSection";
+import ExperienceSection from "./components/ExperienceSection";
+import HeroSection from "./components/HeroSection";
+import ProjectsSection from "./components/ProjectsSection";
+import SkillsSection from "./components/SkillsSection";
+import Navbar from "./components/ui/navbar";
 
 function App() {
-  return <>Hello world</>;
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <HeroSection />
+      <ProjectsSection />
+      <SkillsSection />
+      <ExperienceSection />
+      <ConnectSection />
+    </div>
+  );
 }
 
 export default App;
