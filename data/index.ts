67b73c4db42d56f08d71dd053f2e12ee4194dc7a
Sufiya Sayyed <ssayyed01@gmail.com>
2025-12-03 12:26:37 +0400

    resume link updated

--- data/index.ts
+++ data/index.ts
@@ -187,4 +187,4 @@ export const socialLinks: SocialLinks[] = [
 ];
 
 export const ResumeLink: string =
-  "https://drive.google.com/file/d/1PrrIAkqDln8PjdhocLDAqwIw85TSO3iH/view?usp=sharing";
+  "https://drive.google.com/file/d/1RoHvpluZWKIkAw8pnUipYJhGunWeXVzU/view?usp=sharing";
